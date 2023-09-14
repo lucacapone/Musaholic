4e91cb07bea2d21d8781050f7f46786b5c3fd60d
LUCALAM <lucacapo97@gmail.com>
2023-09-14 13:34:43 +0200

    sonarcloud

--- src/main/java/logic/dao/LessonDAOCSV.java
+++ src/main/java/logic/dao/LessonDAOCSV.java
@@ -15,8 +15,6 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
 
-
-
 public class LessonDAOCSV implements LessonDAO {
 
     private static final String CSV_FILE_NAME = "src/main/resources/file/localDBFile.csv";
@@ -45,7 +43,6 @@ public class LessonDAOCSV implements LessonDAO {
         List<Lesson> lst = new ArrayList<>();
 
 
-
         if (lst.isEmpty()) {
             lst = this.retrieveLessonByIdStudent(this.fd, idStudent);
 
@@ -60,68 +57,69 @@ public class LessonDAOCSV implements LessonDAO {
 
         CSVReader csvReader = new CSVReader(new BufferedReader(new FileReader(fd)));
 
-                String[] record;
+        String[] record;
         List<Lesson> lessonList = new ArrayList<>();
 
-        try{
-        while ((record = csvReader.readNext()) != null) {
-            int posNum= LessonAttributesOrder.ID_STUDENT;
+        try {
+            while ((record = csvReader.readNext()) != null) {
+                int posNum = LessonAttributesOrder.ID_STUDENT;
 
-            boolean recordFound = record[posNum].equals(idStudent);
-            if (recordFound) {
-                String is = record[LessonAttributesOrder.ID_STUDENT];
-                LocalDate d = LocalDate.parse(record[LessonAttributesOrder.DATE]);
-                int t = Integer.parseInt(record[LessonAttributesOrder.TIME]);
-                String mi = record[LessonAttributesOrder.MUSICAL_INSTRUMENT];
-                int p = Integer.parseInt(record[LessonAttributesOrder.PRICE]);
-                String it = record[LessonAttributesOrder.ID_TEACHER];
-                String th = record[LessonAttributesOrder.TEACHER];
-                String c = record[LessonAttributesOrder.CLASSROOM];
-                Lesson lesson= new Lesson();
-                lesson.setIdStudent(is);
-                lesson.setDate(d);
-                lesson.setMusicalInstrument(mi);
-                lesson.setPrice(p);
-                lesson.setIdTeacher(it);
-                lesson.setTeacher(th);
-                lesson.setClassroom(c);
-                lesson.setTime(t);
-                lessonList.add(lesson);
+                boolean recordFound = record[posNum].equals(idStudent);
+                if (recordFound) {
+                    String is = record[LessonAttributesOrder.ID_STUDENT];
+                    LocalDate d = LocalDate.parse(record[LessonAttributesOrder.DATE]);
+                    int t = Integer.parseInt(record[LessonAttributesOrder.TIME]);
+                    String mi = record[LessonAttributesOrder.MUSICAL_INSTRUMENT];
+                    int p = Integer.parseInt(record[LessonAttributesOrder.PRICE]);
+                    String it = record[LessonAttributesOrder.ID_TEACHER];
+                    String th = record[LessonAttributesOrder.TEACHER];
+                    String c = record[LessonAttributesOrder.CLASSROOM];
+                    Lesson lesson = new Lesson();
+                    lesson.setIdStudent(is);
+                    lesson.setDate(d);
+                    lesson.setMusicalInstrument(mi);
+                    lesson.setPrice(p);
+                    lesson.setIdTeacher(it);
+                    lesson.setTeacher(th);
+                    lesson.setClassroom(c);
+                    lesson.setTime(t);
+                    lessonList.add(lesson);
+                }
             }
-        }  if (lessonList.isEmpty()) {
-                        DAOException e = new DAOException("No Lesson found matching with id student: " + idStudent);
-                        throw e;
-                    }}
-
-
-finally{
+            if (lessonList.isEmpty()) {
+                DAOException e = new DAOException("No Lesson found matching with id student: " + idStudent);
+                throw e;
+            }
+        } finally {
             csvReader.close();
         }
         return lessonList;
     }
 
 
-
     @Override
-  public  synchronized void saveLesson(Lesson instance) throws IOException {
+    public synchronized void saveLesson(Lesson instance) throws IOException {
         // create csvWriter object passing file reader as a parameter
         CSVWriter csvWriter = new CSVWriter(new BufferedWriter(new FileWriter(this.fd, true)));
 
 
         String[] record = new String[8];
+        try {
+            record[LessonAttributesOrder.ID_STUDENT] = instance.getIdStudent();
+            record[LessonAttributesOrder.DATE] = String.valueOf(instance.getDate());
+            record[LessonAttributesOrder.TIME] = String.valueOf(instance.getTime());
+            record[LessonAttributesOrder.MUSICAL_INSTRUMENT] = instance.getMusicalInstrument();
+            record[LessonAttributesOrder.PRICE] = String.valueOf(instance.getPrice());
+            record[LessonAttributesOrder.ID_TEACHER] = instance.getIdTeacher();
+            record[LessonAttributesOrder.TEACHER] = instance.getTeacher();
+            record[LessonAttributesOrder.CLASSROOM] = instance.getClassroom();
 
-        record[LessonAttributesOrder.ID_STUDENT] = instance.getIdStudent();
-        record[LessonAttributesOrder.DATE] = String.valueOf(instance.getDate());
-        record[LessonAttributesOrder.TIME] = String.valueOf(instance.getTime());
-        record[LessonAttributesOrder.MUSICAL_INSTRUMENT] = instance.getMusicalInstrument();
-        record[LessonAttributesOrder.PRICE] = String.valueOf(instance.getPrice());
-        record[LessonAttributesOrder.ID_TEACHER] = instance.getIdTeacher();
-        record[LessonAttributesOrder.TEACHER] = instance.getTeacher();
-        record[LessonAttributesOrder.CLASSROOM] = instance.getClassroom();
+            csvWriter.writeNext(record);
+        } finally {
+            csvWriter.flush();
+            csvWriter.close();
+        }
 
-        csvWriter.writeNext(record);
-        csvWriter.flush();
-        csvWriter.close();
     }
 
     private static class LessonAttributesOrder {
