72bafb59166d5c09675618177a427d314eb1d4dc
LUCALAM <lucacapo97@gmail.com>
2023-09-15 12:25:30 +0200

    test

---
+++ src/main/java/test/test_controller/TestBookingLessonController.java
@@ -0,0 +1,23 @@
+package test_controller;
+
+
+import logic.bean.DateBean;
+import logic.bean.MusicalInstrumentBean;
+import logic.bean.PriceBean;
+import logic.bean.TimeBean;
+import logic.controller.BookingLessonController;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBookingLessonController {
+    @Test
+    public void testCheckLessonDetails(){
+        BookingLessonController bookingLessonController = new BookingLessonController();
+        DateBean dateBean = new DateBean();
+        MusicalInstrumentBean musicalInstrumentBean = new MusicalInstrumentBean();
+        PriceBean priceBean = new PriceBean();
+        TimeBean timeBean = new TimeBean();
+        assertEquals(false,bookingLessonController.checkLessonDetails(dateBean,musicalInstrumentBean,priceBean,timeBean));
+       }
+}
